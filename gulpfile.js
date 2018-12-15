1d92fc2b19195e62e205556af63dc509a9ebbe3d
lichRuslan <lichRuslan@users.noreply.github.com>
2018-12-15 16:51:17 +0600

    guip нормально настроин можно начинать создовать шадлон пс: запуск guip если устоновлен глобально

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+const gulp = require ('gulp');
+const scss = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const cssnano = require('gulp-cssnano');
+const braowserSync  =require('browser-sync');
+gulp.task('scss', ()=>{
+    return gulp
+    .src('dev/scss/**/*.scss')
+    .pipe(scss())
+    .pipe(
+        autoprefixer(['last 15 versions', '> 1%','ie 8', 'ie 7']),{
+            cascade: true
+        }
+    )
+    .pipe(cssnano())
+    .pipe(gulp.dest('dist/css'))
+    .pipe(braowserSync.reload({stream:true}));
+});
+
+gulp.task('browser-sync', ()=> {
+    braowserSync({
+        server:{
+            baseDir:'dist'
+        },
+        notify:false
+    })
+});
+
+gulp.task('default', ['browser-sync','scss'],()=> {
+    gulp.watch('dev/scss/**/*.scss', ['scss']);
+    gulp.watch('dist/*.html', braowserSync.reload);
+});
